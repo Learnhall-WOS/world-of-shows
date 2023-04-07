49c0d5575882d11d33a564b6923af678b1ebb938
runo hr <runoharris215@gmail.com>
2023-04-07 14:17:40 -0700

    Add role field to register page. Make sure only theters can post shows

--- base/views.py
+++ base/views.py
@@ -6,8 +6,10 @@ from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm 
+from django import forms
 
-from .models import Show, Genre
+
+from .models import Show, Genre, Talent, Theater
 from dataclasses import dataclass
 from .forms import ShowForm
 
@@ -17,44 +19,30 @@ class G:
     shows = Show.objects
     genres = Genre.objects
 
+class RoleChoiceForm(UserCreationForm):
+    role_choices = [('T', 'Theater'), ('A', 'Talent'), ('O', 'Other')]
+    role = forms.ChoiceField(choices=role_choices, label='Role')
+
+    class Meta:
+        model = User
+        fields = ['username', 'password1', 'password2', 'role']
+
 # Create your views here.
 
-def login_page(request):
-    page = 'login'
-
-    if request.user.is_authenticated:
-        return redirect('home')
-
-    if request.method == 'POST':
-        username = request.POST.get('username').lower()
-        password = request.POST.get('password')
-        try:
-            user = User.objects.get(username=username)
-        except:
-            messages.error(request, 'User does not exist')
-        
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('home')
-        else:
-            messages.error(request, 'Password does not match the username')
-
-    context = {'page': page}
-    return render(request, 'base/login_register.html', context)
-
-def logout_user(request):
-    logout(request)
-    return redirect('home')
-
 def register_page(request):
-    form = UserCreationForm()
+    form = RoleChoiceForm()
     if request.method == 'POST':
-       form = UserCreationForm(request.POST)
+       form = RoleChoiceForm(request.POST)
        if form.is_valid():
            user = form.save(commit=False)
            user.username = user.username.lower()
            user.save()
+           # create a theater or talent object based on user's role
+           if form.cleaned_data['role'] == 'T':
+               Theater.objects.create(user=user, name=user.username)
+           elif form.cleaned_data['role'] == 'A':
+               Talent.objects.create(name=user.username)
+
            login(request, user)
            return redirect('home')
        else:
@@ -63,13 +51,46 @@ def register_page(request):
     context = {'form': form}
     return render(request, 'base/login_register.html', context)
 
+def login_page(request):
+    page = 'login'
+
+    if request.user.is_authenticated:
+        return redirect('home')
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        print(username)
+        try:
+            user = User.objects.get(username=username)
+            
+        except:
+            messages.error(request, 'User does not exist')
+            print("User does not exist")
+        
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Password does not match the username')
+            print("Password does not match the username")
+
+    context = {'page': page}
+    return render(request, 'base/login_register.html', context)
+
+def logout_user(request):
+    logout(request)
+    return redirect('home')
+
+
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
     shows = G.shows.filter(
         Q(genre__name__icontains=q) |
         Q(name__icontains=q) |
         Q(description__icontains=q)
-        )
+        ).distinct()
 
     shows_count = shows.count()    
     genres = G.genres.all()
@@ -84,6 +105,11 @@ def shows(request, pk):
 
 @login_required(login_url='login')
 def post_show(request):
+    # Check if the current user is associated with a theater
+    if not hasattr(request.user, 'theater'):
+        messages.error(request, 'Only theaters can post shows.')
+        return redirect('home')
+    
     form = ShowForm()
     if request.method == 'POST':
         form = ShowForm(request.POST)
@@ -99,7 +125,7 @@ def update_show(request, pk):
     show = G.shows.get(id=pk)
     form = ShowForm(instance=show)
 
-    if request.user != show.host:
+    if request.user != show.host.user:
         return HttpResponse('You are not authorized to update!')
 
     if request.method == 'POST':
@@ -114,7 +140,7 @@ def update_show(request, pk):
 def delete_show(request, pk):
     show = G.shows.get(id=pk)
 
-    if request.user != show.host:
+    if request.user != show.host.user:
         return HttpResponse('You are not authorized to delete!')
     
     if request.method == 'POST':
