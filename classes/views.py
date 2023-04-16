a298d0c265c2007313f6191912c92c043a2294b4
Indrajid <indrajid.lurah@gmail.com>
2023-04-16 20:41:26 +0700

    add search functionality to home.html

--- classes/views.py
+++ classes/views.py
@@ -5,13 +5,23 @@ from django.core.paginator import Paginator
 
 # Create your views here.
 def home(request):
-    classes = Class.objects.all()
+    q=request.GET.get("q")
+    if not q or q == "None" or q=="" :
+        classes = Class.objects.all()
+    else : 
+        classes = Class.objects.filter(
+            Q(name__icontains=q) |
+            Q(owner__user__username__icontains=q) |
+            Q(description__icontains=q) |
+            Q(schedule__icontains=q) |
+            Q(instructor__user__username__icontains=q)
+         ).distinct()
     paginator = Paginator(classes,3)
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
     context = {
+        'query':q,
         'classes':classes,
         'page_obj':page_obj
     }
-
     return render(request,'classes/home.html',context)
