77b384b638e8130fad41909dc8f2d9fc1e0aabf0
runo hr <runoharris215@gmail.com>
2023-04-17 10:48:08 -0700

    Delete base app by renaming it to shows

--- base/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Show, Genre, Review, Talent, Theater, CastMember
-
-admin.site.register([Show, Genre, Review, Talent, Theater])
-
